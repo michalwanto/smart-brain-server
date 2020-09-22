78a9d7d203603817c99f4c286d0f7faaa1a90c4e
michalwanto <59409764+michalwanto@users.noreply.github.com>
2020-09-22 23:16:28 +1000

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -8,7 +8,9 @@ const handleRegister = (req, res, bcrypt, db) => {
   const hash = bcrypt.hashSync(passwords);
   if (name) {
     return db.transaction((trx) =>
-      trx("login").then((output) => res.json(output))
+      trx("login")
+        .then((output) => res.json(output))
+        .catch((err) => res.json("error getting the login data"))
     );
   }
   db.transaction((trx) => {
